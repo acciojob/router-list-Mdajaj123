24eea29f550f90b6322347f6ded1a5e4b83c34a5
unknown <mdijaz2596@gmail.com>
2024-01-08 21:43:09 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {Routes,Route} from 'react-router-dom'
+import Itemlist from './Itemlist'
+import Item1 from './Item1'
+import Item2 from './Item2'
+import Item3 from './Item3'
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Routes>
+        <Route path='/' element={<Itemlist />}/>
+        <Route path='/items/1' element={<Item1/>} />
+        <Route path='/items/2' element={<Item2 />} />
+        <Route path='/items/3' element={<Item3 />} />
+      </Routes>
     </div>
   )
 }
